40970048f69149422c32035dbf120f5369412ad8
Jett <jettc505@gmail.com>
2023-05-01 16:56:03 -0600

    added current line bg hilight

--- lua/jettc/remap.lua
+++ lua/jettc/remap.lua
@@ -21,4 +21,3 @@ vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux-sessionizer<CR>")
 
 vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left>")
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
-
