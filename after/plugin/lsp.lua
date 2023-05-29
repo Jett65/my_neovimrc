653e9e1b7799f60c25be97273acb3346ac49a8e7
Jett <jettc505@gmail.com>
2023-05-28 19:19:30 -0600

    new stuff

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -5,15 +5,40 @@ lsp.on_attach(function(client, bufnr)
 end)
 
 lsp.ensure_installed({
-    -- Replace these with whatever servers you want to install
     'tsserver',
     'eslint',
     'jedi_language_server',
     'lua_ls',
     'cssls',
     'html',
-    'jsonls'
+    'jsonls',
 })
+
+-- custom mappings
+local cmp = require('cmp')
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+local cmp_mappints = lsp.defaults.cmp_mappings {
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-Space'] = cmp.mapping.complete(),
+}
+
+local lsp = require('lsp-zero').preset({})
+
+lsp.on_attach(function(client, bufnr)
+    lsp.default_keymaps({ buffer = bufnr })
+end)
+
+lsp.set_sign_icons({
+    error = '✘',
+    warn = '▲',
+    hint = '⚑',
+    info = '»'
+})
+
+lsp.setup()
+
 
 lsp.on_attach(function(client, bufnr)
     lsp.default_keymaps({ buffer = bufnr })
@@ -33,8 +58,8 @@ end)
 -- })
 
 lsp.on_attach(function(client, bufnr)
-  lsp.default_keymaps({buffer = bufnr})
-  lsp.buffer_autoformat()
+    lsp.default_keymaps({ buffer = bufnr })
+    lsp.buffer_autoformat()
 end)
 
 lsp.setup_nvim_cmp({
