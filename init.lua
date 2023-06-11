b133d4ca39e2c4853ae802e213614e62691cecdf
Jett <jettc505@gmail.com>
2023-06-10 18:51:21 -0600

    +

--- init.lua
+++ init.lua
@@ -1,1 +1,27 @@
 require("jettc")
+
+function MotionHelpH()
+    print("Horizontal movement")
+    print('container = () or {} or "" ... ')
+
+
+    print("---Jumps--")
+    print("f<container> jumps forward to the nearest container in front of the cursor")
+    print('F<container> jumps back to the nearest open container behind the cursor ')
+
+    print("---Selects---")
+
+    print("y can be substituted for v to yank")
+
+    print('<vi>container Selects all inside ')
+    print('<va>container Selects all inside including the container')
+    print("viw Selects word the cursor is on")
+    print("viW Selects what the cursor is on until white spaces")
+
+    print("---Delete---")
+    print("<dt>container delete until container")
+end
+
+function MotionHelpV()
+    print("Vertical movement")
+end
--- lua/jettc/remap.lua
+++ lua/jettc/remap.lua
@@ -21,3 +21,5 @@ vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux-sessionizer<CR>")
 
 vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left>")
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+vim.keymap.set("n", "<M-v>", "<C-v>")
